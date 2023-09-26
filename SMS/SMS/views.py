655ed25eaa76a048b601cbdc0fb29ecef9ab0ab7
Rajesh Kumar Das <103172422+rajeshdiu@users.noreply.github.com>
2023-09-27 03:02:16 +0600

    Update views.py
    send mail

--- SMS/SMS/views.py
+++ SMS/SMS/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
 
 def home(request):
+    send_mail(
+    "Test Mail",
+    "Hello World",
+    "lecturer2.wadp.nsda@dipti.com.bd",
+    ["rajeshcpi1212@gmail.com"],
+    fail_silently=False,
+    )
     return render(request,"index.html")
